eb5d4e495306300d7da3a5c9baf6793e55b83a21
wikiselev <vk6@sanger.ac.uk>
2018-09-16 16:16:26 +0100

    update poststart

--- poststart.sh
+++ poststart.sh
@@ -2,9 +2,5 @@
 
 # copy default templates
 git clone https://github.com/cellgeni/notebooks git-notebooks
-cp -Rf git-notebooks/files/ .
+cp -Rf git-notebooks/files/. .
 rm -rf git-notebooks
-
-# create local R library
-Rscript -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
-Rscript -e '.libPaths( c( Sys.getenv("R_LIBS_USER"), .libPaths() ) )'

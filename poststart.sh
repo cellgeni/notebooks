4723722acc53834b8945c1b94d0cf5e0bcea80da
Anton Khodak <ak27@sanger.ac.uk>
2019-01-15 15:24:31 +0000

    Update poststart.sh

--- poststart.sh
+++ poststart.sh
@@ -4,3 +4,6 @@
 git clone https://github.com/cellgeni/notebooks git-notebooks
 cp -Rf git-notebooks/files/. .
 rm -rf git-notebooks
+if [ ! -d my-conda-envs/myenv ]; then
+    conda create --clone base --name myenv
+fi

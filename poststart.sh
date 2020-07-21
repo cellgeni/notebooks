e583f4fb48fec0affee9cdd4c73011672e444957
prete <martin.prete@sanger.ac.uk>
2020-07-21 13:17:09 +0100

    Update poststart.sh - Create myenv and add kernel - Create Rprofile with binary packages repo - Remove docker - Change ownership of /nfs/kustre/lustre /nfs /warehouse to jovyan - Make .bash_profile source .bashrc

--- poststart.sh
+++ poststart.sh
@@ -1,25 +1,41 @@
 #!/usr/bin/env bash
 
-# copy default templates
-if [ -d git-notebooks ]; then
-    rm -rf git-notebooks
+# copy notebooks
+if [ ! -d .condarc ]; then
+    git clone https://github.com/cellgeni/notebooks /tmp/git-notebooks
+    cp -Rf /tmp/git-notebooks/files/. .
+    rm -rf /tmp/git-notebooks
 fi
-git clone https://github.com/cellgeni/notebooks git-notebooks
-cp -Rf git-notebooks/files/. .
-rm -rf git-notebooks
+
+# create default environment 'myenv'
 if [ ! -d my-conda-envs/myenv ]; then
     conda create --clone base --name myenv
+    source activate myenv
+    python -m ipykernel install --user --name=myenv
+fi
+
+# create .Rprofile to use binary packages
+if [ ! -d .Rprofile ]; then
+    UBUNTU_CODENAME=$(lsb_release -c | awk '{print $2}')
+    REPO_NAME="https://packagemanager.rstudio.com/all/__linux__/$UBUNTU_CODENAME/latest"
+    echo "options(repos = c(REPO_NAME = \"$REPO_NAME\"))" > .Rprofile
 fi
 
 Rscript -e 'dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)'
 Rscript -e '.libPaths( c( Sys.getenv("R_LIBS_USER"), .libPaths() ) )'
+Rscript -e 'IRkernel::installspec()'
 
 # create matching folders to mount the farm
-sudo mkdir -p /nfs
-sudo mkdir -p /lustre
-sudo mkdir -p /warehouse
+if [ ! -d /nfs ] || [ ! -d /lustre ] || [ ! -d /warehouse ]; then
+    sudo mkdir -p /nfs
+    sudo mkdir -p /lustre
+    sudo mkdir -p /warehouse
+    sudo chown -R jovyan /lustre /nfs /warehouse
+fi
 
-# start docker service
-sudo service docker start
+# make login shells source .bashrc
+if [ ! -d .bash_profile ]; then
+    source ~/.bashrc
+fi
 
 export USER=jovyan
